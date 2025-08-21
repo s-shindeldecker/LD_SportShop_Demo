71648691a76cf4845ee84f6e8728f47187bde2db
s-shindeldecker <scott.shindeldecker@gmail.com>
2025-08-21 10:25:38 -0500

    Initial Commit

---
+++ LD_SportShop_Demo/LD_SportShop_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LD_SportShop_DemoApp.swift
+//  LD_SportShop_Demo
+//
+//  Created by Scott Shindeldecker on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct LD_SportShop_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LD_SportShop_DemoTests/LD_SportShop_DemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  LD_SportShop_DemoTests.swift
+//  LD_SportShop_DemoTests
+//
+//  Created by Scott Shindeldecker on 8/21/25.
+//
+
+import Testing
+
+struct LD_SportShop_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
